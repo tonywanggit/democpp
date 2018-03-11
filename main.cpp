1c952f10447166ccc5e9d592b7988dc4b7fd164b
tony <tonywang_efun@hotmail.com>
2018-03-11 10:53:28 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *fp = fopen("/abc", "r");
+    if(fp == NULL)
+    {
+        printf("errno: %d\n", errno);
+        perror("open file /abc");
+        fprintf(stderr, "%s, line:%d: %s\n", __FILE__, __LINE__, strerror(errno));
+        exit(1);
+    }
+
+    return 0;
+}
+
